184949e53e7d32d375e44a235d624db434f78fd6
wynadiis23 <adii.empty@gmail.com>
2019-07-08 20:39:11 +0800

    late push

--- resources/views/errors/500.blade.php
+++ resources/views/errors/500.blade.php
@@ -7,13 +7,12 @@
 
       <div class="error-page">
         <h2 class="headline text-red">500</h2>
-
         <div class="error-content">
           <h3><i class="fa fa-warning text-red"></i> Oops! Something went wrong.</h3>
-
           <p>
             Terjadi kesalah sistem, pastikan <strong>inputan</strong> dan <strong>kode</strong> anda benar. Cek <strong>config server</strong> dan <strong>API</strong> pada Helper. 
           </p>
+          
         </div>
       </div>
       <!-- /.error-page -->
--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -2,6 +2,62 @@
 
 @section('content')
     <div class='row'>
+        <div class="col-lg-3 col-xs-6">
+          <!-- small box -->
+          <div class="small-box bg-aqua">
+            <div class="inner">
+              <h3>150</h3>
+
+              <p>New Orders</p>
+            </div>
+            <div class="icon">
+              <i class="ion ion-bag"></i>
+            </div>
+            <a href="#" class="small-box-footer">More info <i class="fa fa-arrow-circle-right"></i></a>
+          </div>
+        </div>
+        <div class="col-lg-3 col-xs-6">
+          <!-- small box -->
+          <div class="small-box bg-yellow">
+            <div class="inner">
+              <h3>44</h3>
+
+              <p>User Registrations</p>
+            </div>
+            <div class="icon">
+              <i class="ion ion-person-add"></i>
+            </div>
+            <a href="#" class="small-box-footer">More info <i class="fa fa-arrow-circle-right"></i></a>
+          </div>
+        </div>
+        <div class="col-lg-3 col-xs-6">
+          <!-- small box -->
+          <div class="small-box bg-yellow">
+            <div class="inner">
+              <h3>44</h3>
+
+              <p>User Registrations</p>
+            </div>
+            <div class="icon">
+              <i class="ion ion-person-add"></i>
+            </div>
+            <a href="#" class="small-box-footer">More info <i class="fa fa-arrow-circle-right"></i></a>
+          </div>
+        </div>
+        <div class="col-lg-3 col-xs-6">
+          <!-- small box -->
+          <div class="small-box bg-yellow">
+            <div class="inner">
+              <h3>44</h3>
+
+              <p>User Registrations</p>
+            </div>
+            <div class="icon">
+              <i class="ion ion-person-add"></i>
+            </div>
+            <a href="#" class="small-box-footer">More info <i class="fa fa-arrow-circle-right"></i></a>
+          </div>
+        </div>
         <div class='col-md-6'>
             <!-- Box -->
             <div class="box box-primary">
