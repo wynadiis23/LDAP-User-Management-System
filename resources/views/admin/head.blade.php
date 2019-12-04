5e193208fa7ece5bcf936537a035d80dc9e2b5c2
wynadiis23 <adii.empty@gmail.com>
2019-12-04 15:00:26 +0800

    adding title section

--- resources/views/admin/head.blade.php
+++ resources/views/admin/head.blade.php
@@ -1,7 +1,7 @@
 <head>
   <meta charset="utf-8">
   <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <title>AdminLTE 2 | Starter</title>
+  <title>LUM -  @yield("title")</title>
   <!-- Tell the browser to be responsive to screen width -->
   <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
   <link rel="stylesheet" href="{{ asset("/bower_components/bootstrap/dist/css/bootstrap.min.css") }}">
