9e5a324a27142048d24d520d3f2376390585dbcb
wynadiis23 <adii.empty@gmail.com>
2019-12-04 15:01:24 +0800

    remove searchbar and multilevel tidak berguna

--- resources/views/admin/sidebar.blade.php
+++ resources/views/admin/sidebar.blade.php
@@ -16,15 +16,7 @@
       </div>
 
       <!-- search form (Optional) -->
-      <form action="#" method="get" class="sidebar-form">
-        <div class="input-group">
-          <input type="text" name="q" class="form-control" placeholder="Search...">
-          <span class="input-group-btn">
-              <button type="submit" name="search" id="search-btn" class="btn btn-flat"><i class="fa fa-search"></i>
-              </button>
-            </span>
-        </div>
-      </form>
+      
       <!-- /.search form -->
 
       <!-- Sidebar Menu -->
@@ -59,17 +51,7 @@
         </li>
         <li class="header">SERVER</li>
         <li><a href="{{route ('infoserver')}}"><i class="fa fa-info"></i> <span>Info Server</span></a></li>
-        <li class="treeview">
-          <a href="#"><i class="fa fa-link"></i> <span>Multilevel</span>
-            <span class="pull-right-container">
-                <i class="fa fa-angle-left pull-right"></i>
-              </span>
-          </a>
-          <ul class="treeview-menu">
-            <li><a href="#">Link in level 2</a></li>
-            <li><a href="#">Link in level 2</a></li>
-          </ul>
-        </li>
+        
       </ul>
       <!-- /.sidebar-menu -->
     </section>
